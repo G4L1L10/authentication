ded4bd55526e00ba5b2779cc80d6d2ce46d85215
g4l1l10 <axeljaecob@gmail.com>
2025-03-04 16:31:20 +0800

    added localhost address to cors

--- middlewares/cors_middleware.go
+++ middlewares/cors_middleware.go
@@ -10,7 +10,7 @@ import (
 // CORSMiddleware configures Cross-Origin Resource Sharing (CORS) rules
 func CORSMiddleware() gin.HandlerFunc {
 	return cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "https://yourfrontend.com"}, // Update as needed
+		AllowOrigins:     []string{"http://localhost:5173", "https://yourfrontend.com"}, // Update as needed
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Authorization", "Content-Type"},
 		ExposeHeaders:    []string{"Content-Length"},
